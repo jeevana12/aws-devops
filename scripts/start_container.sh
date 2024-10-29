646e1a7c3050b9c2282ea90beec620c0bf5312f2
jeevana12 <143477580+jeevana12@users.noreply.github.com>
2024-10-29 19:41:53 +0530

    added tag for containers

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push jeevana12/simple-python-flask-app
+docker push jeevana12/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 jeevana12/simple-python-flask-app
+docker run -d -p 5000:5000 jeevana12/simple-python-flask-app:latest
 
